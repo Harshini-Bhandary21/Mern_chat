cbb309ab4e7c5cccc936427907a96ddfe9887e41
Harshini Bhandary <harshinibhandary21@gmail.com>
2025-07-11 16:53:38 +0530

    Created 3 models using Mongoose: Chat, User, and Message

---
+++ backend/models/chatModel.js
@@ -0,0 +1,34 @@
+// chatName
+// isGroupChat
+// users
+// latestMessage
+//groupAdmin
+
+import mongoose, { MongooseError } from "mongoose";
+const chatModel = mongoose.Schema(
+    {
+        chatName:{type: String, trim: true},
+        isGroupChat: {type: Boolean, default: false},
+        users:[
+            {
+                type:mongoose.Schema.Types.ObjectId,
+                ref:"User",
+
+            }
+        ],
+        lastestMessage: {
+            type:mongoose.Schema.Types.ObjectId,
+            ref: "Message",
+        },
+        groupAdmin: {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User",
+        }
+    }
+    ,{
+        timestamps:true,
+    }
+)
+
+const Chat = mongoose.model("Chat",chatModel);
+module.exports = Chat;
